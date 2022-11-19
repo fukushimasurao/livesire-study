b77188f2e241cf1fe86b8b59aaa7ff103a301f7e
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-19 23:26:00 +0900

    カウンターの外見だけ追加

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    // public にしたものは、自動でviewに変数がいく。
+    public $counter = 0;
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
