78f6aee7a754f90fb1ef2d88191e5770bc93c90b
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-19 23:27:47 +0900

    カンター処理追加

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -8,6 +8,10 @@ class Counter extends Component
 {
     // public にしたものは、自動でviewに変数がいく。
     public $counter = 0;
+    public function incriment()
+    {
+        $this->counter++;
+    }
     public function render()
     {
         return view('livewire.counter');
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,6 @@
 <div>
     {{-- 親のdiv要素はかならず一個だけ。 --}}
     現在のカウンター : {{ $counter }}
+    <br>
+    <input type="button" wire:click='incriment' value="+1増加させる">
 </div>
