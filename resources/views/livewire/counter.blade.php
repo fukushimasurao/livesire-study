4fad4076eb05783e98f49c916aa987f60ce1693f
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-19 23:45:08 +0900

    メモ

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -5,4 +5,5 @@
     <input type="button" wire:click='increment' value="+1増加させる">
     <input type="button" wire:click='increment(10)' value="+10増加させる">
     <input type="button" wire:click="$set('counter', 0)" value="0にリセットする">
+    メソッドをリセットする場合は、,'$set'を使う
 </div>
