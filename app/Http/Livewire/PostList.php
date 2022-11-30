7ed41d59501b798a6f2eec5db165213fbcc8dcfd
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-30 23:31:57 +0900

    renderに検索処理を全て入れてしまう

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -7,20 +7,31 @@ use Livewire\Component;
 
 class PostList extends Component
 {
-    public $posts;
     public $word;
 
-    public function mount()
-    {
-        $this->posts = Post::get();
-    }
+    // mountや、updatedXxxを使うと、その都度ハイドレート（無駄なSQL）が走るので、render内で対応する。
+    // public $posts;
+    // public function mount()
+    // {
+    //     $this->posts = Post::get();
+    // }
+    // public function updatedWord($val)
+    // {
+    //     $this->posts = Post::where('title', 'LIKE', '%' . $val . '%')->get();
+    // }
 
     public function render()
     {
+        $posts = Post::query()
+        // whenはifと同じ
+        ->when($this->word, fn ($q, $val) => $q->where('title', 'LIKE', '%' . $val . '%'))
+        ->get();
+
         return view(
             'livewire.post-list',
             // ['posts' => Post::get()],
-            ['posts' => $this->posts],
+            // ['posts' => $this->posts],
+            ['posts' => $posts],
         );
     }
 }
