e49986b80d377d9294fe992074cba007169ee95a
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-12-18 00:20:36 +0900

    登録処理追加

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -10,18 +10,40 @@ class PostList extends Component
 {
     use WithPagination;
     public $word;
+    public $title;
+    public $body;
 
+//    livewireのバリデーション
+    protected $rules = [
+        'title' => ['required'],
+        'body' => ['required'],
+    ];
     // 検索されるたびに最初の状態に戻す
     public function updatingWord()
     {
         $this->resetPage();
     }
+
+    public function register()
+    {
+//        validation処理を行う。
+        $this->validate();
+
+//        validation終わったら、以下処理
+        $data = $this->validate();
+        Post::create($data);
+
+//        全てのpublicプロパティをリセットする。
+        $this->reset();
+
+    }
     public function render()
     {
         $posts = Post::query()
+            ->orderByDesc('id')
         // whenはifと同じ
-        ->when($this->word, fn ($q, $val) => $q->where('title', 'LIKE', '%' . $val . '%'))
-        ->paginate(10);
+            ->when($this->word, fn ($q, $val) => $q->where('title', 'LIKE', '%' . $val . '%'))
+            ->paginate(10);
 
         return view(
             'livewire.post-list',
